9387f85dd0172feff4fdbd20c85e0b951b89bb27
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2023-02-08 18:26:31 +0100

    Update options

--- conf_drm.sh
+++ conf_drm.sh
@@ -3,12 +3,12 @@
 prefix=${PREFIX:-/usr}
 
 if test x$1 = x32; then
-    is64bit=disabled
+    is64bit=false
     archdir=i386-linux-gnu
     export CC="gcc -m32"
     export PKG_CONFIG_PATH=/usr/lib/$archdir/pkgconfig
 else
-    is64bit=enabled
+    is64bit=true
     archdir=x86_64-linux-gnu
 fi
 
@@ -20,5 +20,5 @@ cflags="-fno-omit-frame-pointer"
 
 meson build$1 --prefix $prefix --libdir $prefix/lib/$archdir --buildtype debugoptimized \
 	-Dc_args=$cflags -Dc_link_args=$cflags -Dpkg_config_path=$prefix/lib/$archdir/pkgconfig \
-	-Detnaviv=disabled -Dexynos=disabled -Dfreedreno=disabled -Domap=disabled -Dtegra=disabled -Dvc4=disabled \
-	-Dcairo-tests=disabled
+	-Detnaviv=false -Dexynos=false -Dfreedreno=false -Domap=false -Dtegra=false -Dvc4=false \
+	-Dcairo-tests=false
