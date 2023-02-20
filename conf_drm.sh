a298a426ace7e300eafbaaa850130004de3b5e4f
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2023-02-20 16:51:46 +0100

    wip: 32 bit build support

--- conf_drm.sh
+++ conf_drm.sh
@@ -2,14 +2,13 @@
 
 prefix=${PREFIX:-/usr}
 
+top_folder=$(dirname "$0")
 if test x$1 = x32; then
-    is64bit=false
+    cross_compile="--cross-file $top_folder/i386.ini --cross-file $top_folder/cross-build.ini"
     archdir=i386-linux-gnu
-    export CC="gcc -m32"
-    export PKG_CONFIG_PATH=/usr/lib/$archdir/pkgconfig
 else
-    is64bit=true
     archdir=x86_64-linux-gnu
+    cross_compile="--cross-file $top_folder/x86_64.ini --cross-file $top_folder/cross-build.ini"
 fi
 
 rm -r build$1
@@ -18,7 +17,7 @@ set -e
 
 cflags="-fno-omit-frame-pointer"
 
-meson build$1 --prefix $prefix --libdir $prefix/lib/$archdir --buildtype debugoptimized \
+meson build$1 $cross_compile --prefix $prefix --libdir $prefix/lib/$archdir --buildtype debugoptimized \
 	-Dc_args=$cflags -Dc_link_args=$cflags -Dpkg_config_path=$prefix/lib/$archdir/pkgconfig \
 	-Detnaviv=false -Dexynos=false -Dfreedreno=false -Domap=false -Dtegra=false -Dvc4=false \
-	-Dcairo-tests=false
+	-Dcairo-tests=false -Dvalgrind=false -Dintel=false
--- conf_mesa.sh
+++ conf_mesa.sh
@@ -4,18 +4,15 @@ prefix=${PREFIX:-/usr}
 # for general development (optimized with assertions)
 buildtype=${BUILD_TYPE:-debugoptimized}
 
+top_folder=$(dirname "$0")
 if test x$1 = x32; then
     arch=i386-linux-gnu
-    va=disabled
-    buildtype=release
-    profile="-g"
-
-    gallium_drivers=radeonsi
-    others="-Dplatforms=x11 -Dgallium-vdpau=disabled -Dpkg_config_path=${prefix}/lib/$arch/pkgconfig" # -Dbuild-tests=true"
-
-    export CC="gcc -m32"
-    export CXX="g++ -m32"
+    va=false
+    gallium_drivers=${GALLIUM_DRIVERS:-radeonsi}
+    others="-Dplatforms=x11 -Dgallium-vdpau=disabled"
+    cross_compile="--cross-file $top_folder/i386.ini --cross-file $top_folder/cross-build.ini"
 else
+    cross_compile="--cross-file $top_folder/x86_64.ini --cross-file $top_folder/cross-build.ini"
     arch=x86_64-linux-gnu
     va=enabled
 
@@ -43,7 +40,7 @@ rm -r build$1
 
 set -e
 
-meson build$1 --prefix $prefix --libdir $prefix/lib/$arch --buildtype $buildtype -Dlibunwind=disabled -Dglvnd=true \
-	--native-file `dirname $0`/llvm_config_$arch.cfg \
+meson build$1 $cross_compile --prefix $prefix --libdir $prefix/lib/$arch --buildtype $buildtype -Dlibunwind=disabled -Dglvnd=true \
+	-Dpkg_config_path=$PKG_CONFIG_PATH -Dvalgrind=disabled \
 	-Dgallium-drivers=$gallium_drivers -Dvulkan-drivers=$vulkandrv \
-	-Dc_args="$profile" -Dcpp_args="$profile" $repl $others -Dgallium-va=$va -Dvideo-codecs=$videocodecs
+	-Dc_args="$profile" -Dcpp_args="$profile" $repl $others -Dgallium-vdpau=$va -Dgallium-va=$va -Dvideo-codecs=$videocodecs ${EXTRA_OPTIONS}
